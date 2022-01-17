63f21ece6ff94e09005ebebf852bdcd601b29b64
matan-ysayas <matan.ysayas@gmail.com>
2022-01-17 10:29:44 +0200

    practice

--- ShoeStore/Controllers/api/ElegantShoeController.cs
+++ ShoeStore/Controllers/api/ElegantShoeController.cs
@@ -1,9 +1,11 @@
 ﻿using ShoeStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace ShoeStore.Controllers.api
@@ -19,24 +21,83 @@ namespace ShoeStore.Controllers.api
         }
 
         // GET: api/ElegantShoe/5
-        public string Get(int id)
+        public async Task<IHttpActionResult> Get(int id)
         {
-            return "value";
+
+            try
+            {
+                return Ok(await ShoeStoreDB.ElegantShoes.FindAsync(id));
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
+
         // POST: api/ElegantShoe
-        public void Post([FromBody]string value)
+        public async Task<IHttpActionResult> Post([FromBody]ElegantShoe value)
         {
+            try
+            {
+                ShoeStoreDB.ElegantShoes.Add(value);
+                await ShoeStoreDB.SaveChangesAsync();
+                return Ok("item was add");
+                    
+            }catch(SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT: api/ElegantShoe/5
-        public void Put(int id, [FromBody]string value)
+        public async Task<IHttpActionResult> Put(int id, [FromBody] ElegantShoe value)
         {
+            try
+            {
+                ElegantShoe elegantShoe = await ShoeStoreDB.ElegantShoes.FindAsync(id);
+                elegantShoe.Id = value.Id;
+                elegantShoe.CompanyName = value.CompanyName;
+                elegantShoe.HasHeel = value.HasHeel;
+                elegantShoe.HasInStock = value.HasInStock;
+                elegantShoe.Price=value.Price;
+                elegantShoe.Size=value.Size;
+              await ShoeStoreDB.SaveChangesAsync();
+                return Ok("item was update");
+            }catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE: api/ElegantShoe/5
-        public void Delete(int id)
+        public async Task<IHttpActionResult> Delete(int id)
         {
+            try
+            {
+                ShoeStoreDB.ElegantShoes.Remove(await ShoeStoreDB.ElegantShoes.FindAsync(id));
+                await ShoeStoreDB.SaveChangesAsync();
+                return Ok("iream deleted");
+
+            }catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ ShoeStore/Controllers/api/SportShoeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoeStore.Controllers.api
+{
+    public class SportShoeController : ApiController
+    {
+        // GET: api/SportShoe
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SportShoe/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SportShoe
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SportShoe/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SportShoe/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
